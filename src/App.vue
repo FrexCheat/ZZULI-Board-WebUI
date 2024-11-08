5c93a675cfd3246d848a1c02a4fd316d355de405
FrexCheat <admin@frexlink.com>
2024-11-08 11:00:48 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+const theme = {
+  token: {
+    fontFamily: "HarmonyOS SSC, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'",
+  },
+};
+
+</script>
+
+<template>
+  <a-config-provider :theme="theme">
+    <RouterView />
+  </a-config-provider>
+</template>
+
+<style scoped></style>
