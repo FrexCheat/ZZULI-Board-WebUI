b1cb10545db939afa19be36e1e3367048c65b45d
FrexCheat <admin@frexlink.com>
2024-12-04 01:11:38 +0800

    chore: configure auto-importer of unplugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      imports: ['vue', 'vue-router'],
+      dts: 'src/auto-imports.d.ts',
+    })
+  ],
 })
