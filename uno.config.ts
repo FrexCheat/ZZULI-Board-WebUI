d1a67c69b839cef8369b8dc899af4432b20506fe
FrexCheat <admin@frexlink.com>
2024-12-04 22:26:57 +0800

    chore: create unocss config file

---
+++ uno.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'unocss'
+import presetUno from '@unocss/preset-uno'
+import transformerDirectives from '@unocss/transformer-directives'
+import transformerVariantGroup from '@unocss/transformer-variant-group'
+
+export default defineConfig({
+  presets: [presetUno()],
+  transformers: [transformerDirectives(), transformerVariantGroup()],
+})
