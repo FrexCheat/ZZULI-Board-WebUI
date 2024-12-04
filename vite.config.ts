070e7d34ccd2737ba39533bb605e41ea5e5c12fc
FrexCheat <admin@frexlink.com>
2024-12-04 12:49:18 +0800

    build: change rollup build chunk option

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,21 @@ export default defineConfig({
     AutoImport({
       imports: ['vue', 'vue-router'],
       dts: 'src/auto-imports.d.ts',
-    })
+    }),
   ],
+  build: {
+    chunkSizeWarningLimit: 4000,
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'static/js/[name]-[hash].js',
+        entryFileNames: 'static/js/[name]-[hash].js',
+        assetFileNames: 'static/[ext]/[name]-[hash].[ext]',
+        manualChunks(id: string) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString()
+          }
+        },
+      },
+    },
+  },
 })
